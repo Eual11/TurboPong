ec815387c7cd573220c75f8309be40ed1b91444c
Eual11 <uchihaeual11@gmail.com>
2025-01-12 23:28:50 +0300

    added background and functionality

--- end_game.gd
+++ end_game.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	update_win_label("I am shit")
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,3 +12,11 @@ func _process(delta):
 
 func update_win_label(new_text:String):
 	$VBoxContainer/End_Text.text = new_text
+
+
+func _on_menu_btn_pressed():
+	SignalBus.button_pressed.emit(&"endgame_menu")
+
+
+func _on_restart_pressed():
+	SignalBus.button_pressed.emit(&"endgame_restart")
